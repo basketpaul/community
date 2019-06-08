80f41716d011ff47e05641fbdfe84927df27f825
basketpaul <122125597@qq.com>
2019-06-08 20:47:09 +0800

    fix controller and use properties

--- src/main/java/com/swaggyj/community/community/controller/IndexController.java
+++ src/main/java/com/swaggyj/community/community/controller/IndexController.java
@@ -2,9 +2,8 @@ package com.swaggyj.community.community.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class IndexController {
--- src/main/java/com/swaggyj/community/community/provider/GithubProvider.java
+++ src/main/java/com/swaggyj/community/community/provider/GithubProvider.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO){
         MediaType mediaType = MediaType.get("application/json; charset=utf-8");
-
         OkHttpClient client = new OkHttpClient();
 
         RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
@@ -33,7 +32,7 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token" + accessToken)
+                .url("https://api.github.com/user?access_token=" + accessToken)
                 .build();
 
         try {
