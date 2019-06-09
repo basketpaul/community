fdeac6dffb653351eac6266ca72e185be50f24ab
basketpaul <122125597@qq.com>
2019-06-09 14:33:29 +0800

    add session homepage display username use http request

--- src/main/java/com/swaggyj/community/community/controller/AuthorizeCotroller.java
+++ src/main/java/com/swaggyj/community/community/controller/AuthorizeCotroller.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeCotroller {
     @Autowired
@@ -24,7 +26,8 @@ public class AuthorizeCotroller {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                            HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,7 +36,13 @@ public class AuthorizeCotroller {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if(user != null){
+            request.getSession().setAttribute("user",user);//将当前用户放进session
+            //登录成功，写cookie/session
+            return "redirect:/";
+        }else{
+            //登陆失败，重新登录,暂时先跳回主页
+            return "redirect:/";
+        }
     }
 }
