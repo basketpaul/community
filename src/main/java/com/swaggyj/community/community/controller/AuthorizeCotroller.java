7099fb0ae400de0be87b89c3b8f0ce511df748ee
paul <122125597@qq.com>
2019-06-16 13:37:23 +0800

    加入问题发布功能，使用数据库存储问题内容等

--- src/main/java/com/swaggyj/community/community/controller/AuthorizeCotroller.java
+++ src/main/java/com/swaggyj/community/community/controller/AuthorizeCotroller.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -34,7 +33,6 @@ public class AuthorizeCotroller {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                            HttpServletRequest request,
                             HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -44,14 +42,14 @@ public class AuthorizeCotroller {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        if(githubUser != null){
+        if (githubUser != null && githubUser.getId() != null) {
             User user = new User();
             String token = UUID.randomUUID().toString();
             user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtModified());
+            user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             response.addCookie((new Cookie("token",token)));
             return "redirect:/";
--- src/main/java/com/swaggyj/community/community/controller/PublishController.java
+++ src/main/java/com/swaggyj/community/community/controller/PublishController.java
@@ -29,12 +29,28 @@ public class PublishController {
 
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam("title") String title,
-            @RequestParam("description") String description,
-            @RequestParam("tag") String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
             HttpServletRequest request,
-            Model model
-    ) {
+            Model model) {
+
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
 
         User user = null;
         Cookie[] cookies = request.getCookies();
@@ -49,6 +65,7 @@ public class PublishController {
         }
         if (user == null) {
             model.addAttribute("error", "用户未登录");
+            return "publish";
         }
 
         Question question = new Question();
@@ -60,6 +77,6 @@ public class PublishController {
         question.setGmtModified(question.getGmtCreate());
 
         questionMapper.create(question);
-        return "publish";
+        return "redirect:/";
     }
 }
