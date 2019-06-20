54de2bfff02975c684f8a5880acc8ee07d0a1edc
paul <122125597@qq.com>
2019-06-20 21:11:53 +0800

    增加分页显示功能,limit start,offset

--- src/main/java/com/swaggyj/community/community/controller/IndexController.java
+++ src/main/java/com/swaggyj/community/community/controller/IndexController.java
@@ -1,7 +1,7 @@
 package com.swaggyj.community.community.controller;
 
 
-import com.swaggyj.community.community.dto.QuestionDTO;
+import com.swaggyj.community.community.dto.PaginationDTO;
 import com.swaggyj.community.community.mapper.UserMapper;
 import com.swaggyj.community.community.model.User;
 import com.swaggyj.community.community.service.QuestionService;
@@ -9,10 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 
 @Controller
@@ -26,7 +26,10 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request,
-                        Model model) {
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size
+    ) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie : cookies) {
@@ -39,8 +42,8 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions", questionList);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
         return "index";
     }
 }
