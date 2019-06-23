b90f9f015e3d0daef37877b16776896debdc19c3
paul <122125597@qq.com>
2019-06-23 13:24:23 +0800

    增加拦截器，修复一些问题

--- src/main/java/com/swaggyj/community/community/controller/IndexController.java
+++ src/main/java/com/swaggyj/community/community/controller/IndexController.java
@@ -2,8 +2,6 @@ package com.swaggyj.community.community.controller;
 
 
 import com.swaggyj.community.community.dto.PaginationDTO;
-import com.swaggyj.community.community.mapper.UserMapper;
-import com.swaggyj.community.community.model.User;
 import com.swaggyj.community.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,37 +9,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model,
+    public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
-                        @RequestParam(name = "size", defaultValue = "5") Integer size
-    ) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
--- src/main/java/com/swaggyj/community/community/controller/PublishController.java
+++ src/main/java/com/swaggyj/community/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.swaggyj.community.community.controller;
 
 import com.swaggyj.community.community.mapper.QuestionMapper;
-import com.swaggyj.community.community.mapper.UserMapper;
 import com.swaggyj.community.community.model.Question;
 import com.swaggyj.community.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish() {
@@ -52,17 +47,7 @@ public class PublishController {
             return "publish";
         }
 
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                String token = cookie.getValue();
-                user = userMapper.findByToken(token);
-                if (user != null)
-                    request.getSession().setAttribute("user", user);
-                break;
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             model.addAttribute("error", "用户未登录");
             return "publish";
