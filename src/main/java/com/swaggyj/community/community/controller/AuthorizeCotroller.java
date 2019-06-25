7973f82f9233b662dfe2ac0e211c806410da13a4
paul <122125597@qq.com>
2019-06-25 08:58:23 +0800

    改进登录bug

--- src/main/java/com/swaggyj/community/community/controller/AuthorizeCotroller.java
+++ src/main/java/com/swaggyj/community/community/controller/AuthorizeCotroller.java
@@ -2,9 +2,9 @@ package com.swaggyj.community.community.controller;
 
 import com.swaggyj.community.community.dto.AccessTokenDTO;
 import com.swaggyj.community.community.dto.GithubUser;
-import com.swaggyj.community.community.mapper.UserMapper;
 import com.swaggyj.community.community.model.User;
 import com.swaggyj.community.community.provider.GithubProvider;
+import com.swaggyj.community.community.service.UserService;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -30,7 +31,7 @@ public class AuthorizeCotroller {
     private String redirectUrl;
 
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
@@ -50,15 +51,23 @@ public class AuthorizeCotroller {
             user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
-            user.setGmtCreate(System.currentTimeMillis());
-            user.setGmtModified(user.getGmtCreate());
             user.setAvatarUrl(githubUser.getAvatar_url());
-            userMapper.insert(user);
+            userService.createOrUpdate(user);
             response.addCookie((new Cookie("token",token)));
             return "redirect:/";
         }else{
             //登陆失败，重新登录,暂时先跳回主页
             return "redirect:/";
         }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request,
+                         HttpServletResponse response) {
+        request.getSession().removeAttribute("user");
+        Cookie cookie = new Cookie("token", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
