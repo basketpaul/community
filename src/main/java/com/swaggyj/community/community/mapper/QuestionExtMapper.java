489c21f831d541069ee72fdac9cf398385f1cafc
paul <122125597@qq.com>
2019-07-16 19:47:35 +0800

    添加相关问题功能

--- src/main/java/com/swaggyj/community/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/swaggyj/community/community/mapper/QuestionExtMapper.java
@@ -2,8 +2,12 @@ package com.swaggyj.community.community.mapper;
 
 import com.swaggyj.community.community.model.Question;
 
+import java.util.List;
+
 public interface QuestionExtMapper {
     int incView(Question question);
 
     int incCommentCount(Question record);
+
+    List<Question> selectRelated(Question question);
 }
