3d87098497472c5213aea18e3a009ea1f7eb4569
paul <122125597@qq.com>
2019-07-16 20:39:25 +0800

    规范标签内容

--- src/main/java/com/swaggyj/community/community/controller/PublishController.java
+++ src/main/java/com/swaggyj/community/community/controller/PublishController.java
@@ -1,9 +1,11 @@
 package com.swaggyj.community.community.controller;
 
+import com.swaggyj.community.community.cache.TagCache;
 import com.swaggyj.community.community.dto.QuestionDTO;
 import com.swaggyj.community.community.model.Question;
 import com.swaggyj.community.community.model.User;
 import com.swaggyj.community.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,11 +30,14 @@ public class PublishController {
         model.addAttribute("description", question.getDescription());
         model.addAttribute("tag", question.getTag());
         model.addAttribute("id", question.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
+
     @GetMapping("/publish")
-    public String publish() {
+    public String publish(Model model) {
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -44,10 +49,10 @@ public class PublishController {
             @RequestParam(value = "id", required = false) Long id,
             HttpServletRequest request,
             Model model) {
-
         model.addAttribute("title", title);
         model.addAttribute("description", description);
         model.addAttribute("tag", tag);
+        model.addAttribute("tags", TagCache.get());
 
         if (title == null || title == "") {
             model.addAttribute("error", "标题不能为空");
@@ -59,6 +64,12 @@ public class PublishController {
         }
         if (tag == null || tag == "") {
             model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)) {
+            model.addAttribute("error", "输入非法标签:" + invalid);
             return "publish";
         }
 
