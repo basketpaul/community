72e34a4b1a1b160c72acae34cb7c853ef56a6bca
paul <122125597@qq.com>
2019-07-17 12:58:10 +0800

    完善通知功能，显示未读消息数量

--- src/main/java/com/swaggyj/community/community/interceptor/SessionInterceptor.java
+++ src/main/java/com/swaggyj/community/community/interceptor/SessionInterceptor.java
@@ -3,6 +3,7 @@ package com.swaggyj.community.community.interceptor;
 import com.swaggyj.community.community.mapper.UserMapper;
 import com.swaggyj.community.community.model.User;
 import com.swaggyj.community.community.model.UserExample;
+import com.swaggyj.community.community.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,9 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private NotificationService notificationService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
@@ -33,6 +37,8 @@ public class SessionInterceptor implements HandlerInterceptor {
                     List<User> users = userMapper.selectByExample(userExample);
                     if (users.size() != 0) {
                         request.getSession().setAttribute("user", users.get(0));
+                        Long unreadCount = notificationService.unreadCount(users.get(0).getId());
+                        request.getSession().setAttribute("unreadCount", unreadCount);
                     }
                     break;
                 }
